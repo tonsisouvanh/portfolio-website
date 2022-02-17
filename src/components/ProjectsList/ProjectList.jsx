577c2206f01bef9354ae4e86c2c10c72930ecf61
tonsisouvanh <tonsisouvanh7@gmail.com>
2022-02-17 10:38:57 +0700

    added responsive project page

--- src/components/ProjectsList/ProjectList.jsx
+++ src/components/ProjectsList/ProjectList.jsx
@@ -1,36 +1,35 @@
-import React, { useState } from "react";
-import "./Styles.css";
-import ProjectCard from "./Project/ProjectCard";
-import { Grid } from "../../style/GridStyle";
-import styled from "styled-components";
-function ProjectList({ data }) {
-  const [projects] = useState(data);
-  return (
-    <>
-      <div className="project-list-container">
-        <h1>My Recent Projects</h1>
-        <p>
-          Here are a few design projects I've worked on recently. Want to see
-          more? Email me.
-        </p>
-        <GridContainer>
-          <ProjectCard projects={projects} />
-        </GridContainer>
-      </div>
-    </>
-  );
-}
+// import React, { useState } from "react";
+// import "./Styles.css";
+// import ProjectCard from "./Project/ProjectCard";
+// import styled from "styled-components";
+// function ProjectList({ data }) {
+//   const [projects] = useState(data);
+//   return (
+//     <>
+//       <div className="project-list-container">
+//         <h1>My Recent Projects</h1>
+//         <p>
+//           Here are a few design projects I've worked on recently. Want to see
+//           more? Email me.
+//         </p>
+//         <GridContainer>
+//           <ProjectCard projects={projects} />
+//         </GridContainer>
+//       </div>
+//     </>
+//   );
+// }
 
-const GridContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(4, minmax(0,1fr));
-  grid-template-rows: auto;
-  grid-gap: 1rem;
-  div {
-    background-color: red;
-    width: 20rem;
-    height: 30rem;
-  }
-`;
+// const GridContainer = styled.div`
+//   display: grid;
+//   grid-template-columns: repeat(4, minmax(0,1fr));
+//   grid-template-rows: auto;
+//   grid-gap: 1rem;
+//   div {
+//     background-color: red;
+//     width: 20rem;
+//     height: 30rem;
+//   }
+// `;
 
-export default ProjectList;
+// export default ProjectList;
